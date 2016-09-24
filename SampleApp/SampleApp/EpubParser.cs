e631659769437080885ca40a93d945b722a9f4aa
Anubhav <anubhav92@outlook.com>
2016-09-24 21:28:55 +0530

    Added sample app

---
+++ SampleApp/SampleApp/EpubParser.cs
@@ -0,0 +1,149 @@
+﻿using EpubReader;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace SampleApp
+{
+    class EpubParser
+    {
+        public StorageFile file;
+
+        private Epub document;
+        private int _pageCount;
+        private int _curPageNum;
+
+        private List<string> textList = new List<string>();
+        private List<List<byte>> formatList = new List<List<byte>>();
+
+        public EpubParser(StorageFile _file)
+        {
+            file = _file;
+        }
+
+        public async Task loadFile()
+        {
+            Stream stream = await file.OpenStreamForReadAsync();
+            document = new Epub(stream);
+            _pageCount = document.Content.Count;
+            _curPageNum = -1;
+
+            ProcessEpub();
+        }
+
+        private void ProcessEpub()
+        {
+            for (int i = 0; i < document.Content.Count; i++)
+            {
+                Tuple<string, List<byte>> content = getText(document.Content[i] as ContentData);
+                int prevPos = 0;
+                string text = "";
+                int pos;
+
+                do
+                {
+                    try
+                    {
+                        pos = content.Item1.IndexOf('\n', prevPos + 4000);
+                        if (pos == -1)
+                        {
+                            pos = content.Item1.Length - 1;
+                        }
+                    }
+                    catch
+                    {
+                        pos = content.Item1.Length - 1;
+                    }
+                    text = content.Item1.Substring(prevPos, pos - prevPos + 1);
+
+                    if (text.Trim() != "")
+                    {
+                        textList.Add(text);
+                        formatList.Add(content.Item2.GetRange(prevPos, pos - prevPos + 1));
+                    }
+
+                    prevPos += text.Length - 1;
+                } while (prevPos < content.Item1.Length - 1);
+            }
+            _pageCount = textList.Count;
+        }
+        public bool isPageInRange(int pageNum)
+        {
+            if (pageNum >= 0 && pageNum < _pageCount)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public int pageCount()
+        {
+            return _pageCount;
+        }
+        public int curPageNum()
+        {
+            return _curPageNum;
+        }
+        public Tuple<string, List<byte>> getTextOfNextPage()
+        {
+            if (_curPageNum < _pageCount - 1)
+            {
+                return getText_internal(++_curPageNum);
+            }
+            return null;
+        }
+        public Tuple<string, List<byte>> getTextOfPage(int pageNum)
+        {
+            if (pageNum >= 0 && pageNum < _pageCount)
+            {
+                _curPageNum = pageNum;
+                return getText_internal(_curPageNum);
+            }
+            return null;
+        }
+        public Tuple<string, List<byte>> getTextofPrevPage()
+        {
+            if (_curPageNum > 0)
+            {
+                return getText_internal(--_curPageNum);
+            }
+            return null;
+        }
+        public string getJustText(int pageNum)
+        {
+            if (pageNum >= 0 && pageNum < _pageCount)
+            {
+                return getText_internal(pageNum).Item1;
+            }
+            return null;
+        }
+
+        public string getExtension()
+        {
+            return "epub";
+        }
+
+        public void Destroy()
+        {
+            file = null;
+            document = null;
+        }
+
+        private Tuple<string, List<byte>> getText(ContentData content)
+        {
+            return content.GetContentAsFormattedText();
+        }
+
+        private Tuple<string, List<byte>> getText_internal(int pagenum)
+        {
+            return new Tuple<string, List<byte>>(textList[pagenum], formatList[pagenum]);
+        }
+    }
+}
---
+++ SampleApp/SampleApp/MainPage.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace SampleApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public StorageFile file;
+        private EpubParser fileParser;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void loadFile_clicked(object sender, RoutedEventArgs e)
+        {
+            FileOpenPicker openPicker = new FileOpenPicker();
+            openPicker.ViewMode = PickerViewMode.Thumbnail;
+            openPicker.SuggestedStartLocation = PickerLocationId.ComputerFolder;
+
+            openPicker.FileTypeFilter.Add(".epub");
+
+            file = await openPicker.PickSingleFileAsync();
+
+            if (file != null)
+            {
+                fileParser = new EpubParser(file);
+                await fileParser.loadFile();
+
+                Tuple<string, List<byte>> temp_text = fileParser.getTextOfNextPage();
+                List<byte> BoldList;
+
+                if (temp_text != null)
+                {
+                    BoldList = temp_text.Item2;
+                    tb.Text = temp_text.Item1;
+                }
+
+                next.IsEnabled = prev.IsEnabled = true;
+                t_total.Text = fileParser.pageCount().ToString();
+                t_cur.Text = (fileParser.curPageNum() + 1).ToString();
+            }
+        }
+
+        private void nextPage_click(object sender, RoutedEventArgs e)
+        {
+            Tuple<string, List<byte>> temp_text = fileParser.getTextOfNextPage();
+
+            if (temp_text != null)
+            {
+                tb.Text = temp_text.Item1;
+                t_cur.Text = (fileParser.curPageNum() + 1).ToString();
+            }
+        }
+
+        private void prevPage_click(object sender, RoutedEventArgs e)
+        {
+            Tuple<string, List<byte>> temp_text = fileParser.getTextofPrevPage();
+
+            if (temp_text != null)
+            {
+                tb.Text = temp_text.Item1;
+                t_cur.Text = (fileParser.curPageNum() + 1).ToString();
+            }
+        }
+    }
+}
